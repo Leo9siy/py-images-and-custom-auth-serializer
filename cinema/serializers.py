95f4779d751de168b3893319e029e91c2436811f
Leo <leonsiypopov@gmail.com>
2025-04-15 20:12:16 +0300

    done3

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -79,7 +79,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
     )
     tickets_available = serializers.IntegerField(read_only=True)
 
-    movie_image = serializers.CharField(
+    movie_image = serializers.ImageField(
         source="movie.image", read_only=True
     )
 
